0ebf0cf3763b446f3ab694cb36f4cf88f5ecb03e
Samuel-Lima-dev <samueldelima340@gmail.com>
2025-07-16 18:32:42 -0300

    Convertendo minhas views de function-based view para class-based views para melhor organização

--- app/urls.py
+++ app/urls.py
@@ -3,14 +3,14 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from cars.views import car_views, car_registration
+from cars.views import CarView, CarRegistrationView
 from accounts.views import user_registration, login_view, logout_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', car_views, name='car_list'),
-    path('newcar/', car_registration, name='newcar'),
+    path('index/', CarView.as_view(), name='car_list'),
+    path('newcar/', CarRegistrationView.as_view(), name='newcar'),
 
     path('register_user/', user_registration, name='register_user'),
     path('login/', login_view, name='login'),
--- cars/views.py
+++ cars/views.py
@@ -2,28 +2,43 @@ from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.form import CarModelForm
 
+from django.views import View
 
-# Consulta os carros do banco e retrona para pagina html
+
+
+# Consulta os carros do banco e retorna para pagina html
 # Consulta de forma filtrada através da buscaa do ususario
-def car_views(request):
-    car = Car.objects.all().order_by('model') ##busca todos os carros
+class CarView(View):
+    
+    def get(self, request):
+        cars = Car.objects.all().order_by('model')##busca todos os carros
 
-    ## Busar carros por busca
-    search = request.GET.get('search')
-    if search: 
-        car = car.filter(model__icontains=search)
+        ## pega dados da busca do usuario
+        search = request.GET.get('search')  
+        if search:
+            cars = cars.filter(model_icontains=search)
 
-    return render(
-        request,
-        'home_page.html',
-        {'cars': car}   
-    )
+        return render(
+            request,
+            'home_page.html',
+            {'cars': cars}
+        )
+    
 
+class CarRegistrationView(View):
 
-# 
-def car_registration(request):
+    def get(self, request):
+        # Formulario de cadastro de veiculo
+        new_car_form = CarModelForm()
 
-    if request.method == 'POST':
+        return render(
+            request,
+            'new_car.html',
+            {'new_car_form': new_car_form}
+        )
+    
+
+    def post(self, request):
         #pegando os dados enviado no formulario, através do método post 
         # (request.files ) para quando o formulario estivar o atributo:
         # encytype="multipart/form-data"
@@ -33,13 +48,12 @@ def car_registration(request):
         if new_car_form.is_valid():
             new_car_form.save()
             return redirect('newcar') # redirecionar para a pagina newcar após os dados serem salvos
-    else:
-        new_car_form = CarModelForm()
-    
+        
+        return render(
+            request,
+            'new_car.html',
+            {'new_car_form': new_car_form}
+        )
 
-    return render(
-        request,
-        'new_car.html',
-        {'new_car_form': new_car_form}
-    )
+
 
